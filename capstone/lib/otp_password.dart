2f57d9b80da4bbff9e7e08018c82154e7944d851
alfianadis <alfian.adiseptianto.tik20@mhsw.pnj.ac.id>
2023-05-23 13:40:36 +0700

    merapihkan halaman forgot password

--- capstone/lib/otp_password.dart
+++ capstone/lib/otp_password.dart
@@ -1,120 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pinput/pinput.dart';
 
-class Test extends StatelessWidget {
+import 'components/color_path.dart';
+
+class OtpPassword extends StatefulWidget {
+  const OtpPassword({super.key});
+
+  @override
+  State<OtpPassword> createState() => _OtpPasswordState();
+}
+
+class _OtpPasswordState extends State<OtpPassword> {
+  FocusNode _focus = FocusNode();
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _focus.addListener(_onFocusChange);
+    _focus.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _focus.removeListener(_onFocusChange);
+    _focus.dispose();
+  }
+
+  void _onFocusChange() {
+    if (_focus.hasFocus) {
+      setState(() {
+        height = 20;
+      });
+    } else {
+      setState(() {
+        height = 100;
+      });
+    }
+    debugPrint(height.toString());
+  }
+
+  double height = 50;
+  double width = 0;
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 0);
+    var width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Center(
-          child: ElevatedButton(
-            child: Text('BottomSheet'),
-            onPressed: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                context: context,
-                backgroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusDirectional.only(
-                    topEnd: Radius.circular(25),
-                    topStart: Radius.circular(25),
+      backgroundColor: ColorPath.background,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 40,
+              left: 10,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(
+                    top: 100,
+                  ), // Mengatur posisi gambar dengan angka
+                  // Mengatur margin bawah (opsional)
+                  child: Image.asset(
+                    'assets/images/homelogo.png',
+                    width: 200, // Sesuaikan dengan ukuran yang diinginkan
+                    height: 200, // Sesuaikan dengan ukuran yang diinginkan
                   ),
                 ),
-                builder: (context) => Container(
-                  padding: EdgeInsetsDirectional.only(
-                    start: 20,
-                    end: 20,
-                    bottom: 30,
-                    top: 8,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        height: 600,
-                        child: PageView(
-                          controller: controller,
-                          children: <Widget>[
-                            SingleChildScrollView(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: List.generate(
-                                  5,
-                                  (index) => Container(
-                                    height: 60,
-                                    margin:
-                                        EdgeInsetsDirectional.only(bottom: 10),
-                                    width: double.infinity,
-                                    color: Colors.yellow,
-                                    child: Center(
-                                      child: Text(
-                                        'Widgets $index',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: height * 2.5),
+              // height: height * 0.85,
+              decoration: const BoxDecoration(
+                color: ColorPath.primary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: Expanded(
+                child: ListView(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text(
+                        'Lupa Kata Sandi',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: ColorPath.textcolor1,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(5.0),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: 149,
+                            height: 149,
+                            decoration: BoxDecoration(
+                              color: Color(0xFF2D9CDB),
+                              shape: BoxShape.circle,
                             ),
-                            SingleChildScrollView(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: List.generate(
-                                  10,
-                                  (index) => Container(
-                                    height: 60,
-                                    margin:
-                                        EdgeInsetsDirectional.only(bottom: 10),
-                                    width: double.infinity,
-                                    color: Colors.yellow,
-                                    child: Center(
-                                      child: Text(
-                                        'Widgets $index',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
+                          ),
+                          Image.asset(
+                            'assets/images/Lock.png',
+                            width: 200,
+                            height: 200,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Text('Masukkan Kode OTP',
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: ColorPath.background,
+                          )),
+                    ),
+                    SizedBox(height: 40),
+                    Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          // Navigator.push(context,
+                          //     MaterialPageRoute(builder: (context) => Test()));
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: ColorPath.background,
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: const Text(
+                            'Kirim',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
                             ),
-                            SingleChildScrollView(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: List.generate(
-                                  12,
-                                  (index) => Container(
-                                    height: 60,
-                                    margin:
-                                        EdgeInsetsDirectional.only(bottom: 10),
-                                    width: double.infinity,
-                                    color: Colors.yellow,
-                                    child: Center(
-                                      child: Text(
-                                        'Widgets $index',
-                                        style: TextStyle(fontSize: 20),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              );
-            },
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
