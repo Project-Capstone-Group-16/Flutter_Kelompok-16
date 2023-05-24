f93d46f34ae7b3f2bfa750958710467f1d78cd30
alfianadis <alfian.adiseptianto.tik20@mhsw.pnj.ac.id>
2023-05-24 20:54:53 +0700

    menambahkan slider pada landing page

--- capstone/lib/landing_page.dart
+++ capstone/lib/landing_page.dart
@@ -12,35 +12,66 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   final List<String> imgList = [
     'assets/images/landingpage1.png',
+    'assets/images/landingpage2.png',
     'assets/images/landingpage1.png',
-    'assets/images/landingpage1.png',
-    'assets/images/landingpage1.png'
+    'assets/images/landingpage2.png'
   ];
+
+  final CarouselController _controller = CarouselController();
+
+  int _currentSlide = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        final double height = MediaQuery.of(context).size.height;
-        return CarouselSlider(
-          options: CarouselOptions(
-            height: height,
-            viewportFraction: 1.0,
-            enlargeCenterPage: false,
-            // autoPlay: false,
-          ),
-          items: imgList
-              .map((item) => Container(
-                    child: Center(
-                        child: Image.network(
+    final double height = MediaQuery.of(context).size.height;
+    return Column(
+      children: [
+        Expanded(
+          child: CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              autoPlay: true,
+              onPageChanged: (index, _) {
+                setState(() {
+                  _currentSlide = index;
+                });
+              },
+            ),
+            carouselController: _controller,
+            items: imgList.map((item) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
                       item,
                       fit: BoxFit.cover,
                       height: height,
-                    )),
-                  ))
-              .toList(),
-        );
-      },
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imgList.map((item) {
+            int index = imgList.indexOf(item);
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentSlide == index ? Colors.blue : Colors.grey,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
