233f8b3d853e0f0a4caba3e7bab41467aab9c611
limtobias09 <tobiasssalim@stuents.unnes.ac.id>
2023-05-29 20:42:09 +0700

    Penambahan App Route

--- capstone/lib/route/app_route.dart
+++ capstone/lib/route/app_route.dart
@@ -5,4 +5,18 @@ class AppRoute{
   static const String register_page='/4';
   static const String lupa_password='/5';
   static const String dashboard_screen='/6';
+  static const String explore_screen='/7';
+  static const String halamanukuranbarang_screen='/8';
+  static const String hargabarang_screen='/9';
+  static const String cariloker_screen='/10';
+  static const String lokertersedia_screen='/11';
+  static const String lokerfavorite_screen='/12';
+  static const String durasipenitipan_screen='/13';
+  static const String categorybarang_screen='/14';
+  static const String prosespemesanan_screen='/15';
+  static const String promojasapenitipan_screen='/16';
+  static const String promopengiriman_screen='/17';
+  static const String barangberhasildisimpan_screen='/17';
+  static const String trackingbarang_screen='/18';
+  static const String halamannotifikasi_screen='/18';
 }
