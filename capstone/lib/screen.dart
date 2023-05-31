8232e989dbad86ddfffe75c55bb2a3a387e96f79
limtobias09 <tobiasssalim@stuents.unnes.ac.id>
2023-05-31 17:40:35 +0700

    Halaman Estimasi Biaya
    Berhasil ditambahkan tombol back dan judul estimasi pada halaman estimasi biaya. Selain itu, ditambahkan juga gambar sesuai dari design tim ui/ux. Untuk saat ini baru terdapat satu container yang nantinya akan diisi text dan tombol untuk ke halaman selanjutnya.

--- capstone/lib/screen.dart
+++ capstone/lib/screen.dart
@@ -12,3 +12,4 @@ export 'view/home/home_screen.dart';
 export 'view/explore/explore_screen.dart';
 export 'view/profile/profile_screen.dart';
 export 'view/categoryPage/category_screen.dart';
+export 'view/estimasiBiayaPage/estimasi_biaya.dart';
---
+++ capstone/lib/view/estimasiBiayaPage/estimasi_biaya.dart
@@ -0,0 +1,84 @@
+import 'package:capstone/components/color_path.dart';
+import 'package:flutter/material.dart';
+
+class EstimasiBiayaScreen extends StatelessWidget {
+  const EstimasiBiayaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorPath.primary,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Positioned(
+                  top: 35,
+                  left: 10,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+
+                const SizedBox(width: 100),
+
+                const Text(
+                  'Estimasi',
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: ColorPath.textcolor1,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+
+            const SizedBox(height: 30,),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0, bottom: 50.0),
+              child: Image.asset('assets/images/estimasi.png'),
+            ),
+            
+            Column(
+              children: [
+                const SizedBox(height: 260,),
+                Container(
+                  alignment: Alignment.center,
+                  child: Stack(
+                    children: [
+                      Container(
+                      width: 348,
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.2,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              ),
+                      ),
+                    ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        )
+        ),
+    );
+  }
+}
