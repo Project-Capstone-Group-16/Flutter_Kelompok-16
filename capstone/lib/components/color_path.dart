badc7bb65dea4454c438306243b5eacbc5c6c655
alfianadis <alfian.adiseptianto.tik20@mhsw.pnj.ac.id>
2023-05-31 17:25:12 +0700

    test

--- capstone/lib/components/color_path.dart
+++ capstone/lib/components/color_path.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ColorPath {
   static const Color background = Color(0xFF2D64FB);
   static const Color background2 = Color(0xFFEEF9F7);
+  static const Color AppbarProfile = Color(0xFF1652F9);
   static const Color primary = Color(0xFFEEF9F7);
   static const Color textcolor1 = Color(0xFF1652F9);
   static const Color grey = Color(0xFF898989);
--- capstone/lib/view/profile/profile_screen.dart
+++ capstone/lib/view/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:capstone/components/color_path.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -11,8 +12,65 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile Screen'),
+      backgroundColor: ColorPath.background2,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: const AlignmentDirectional(-1, -1),
+              child: Container(
+                width: 448,
+                height: 120,
+                decoration: const BoxDecoration(
+                  color: ColorPath.AppbarProfile,
+                ),
+              ),
+            ),
+            Align(
+              alignment: AlignmentDirectional(0, -0.89),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: AlignmentDirectional(-0.9, -0.92),
+                    child: Text(
+                      'Profil',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: ColorPath.background2,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional(0.6, -0.94),
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.network(
+                        'https://picsum.photos/seed/73/600',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional(0.9, -0.93),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.logout,
+                          color: ColorPath.background2,
+                          size: 24,
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
