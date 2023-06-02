af75008feeef33bf375a4cd7fc92ce440c6670fc
Rainmakers21 <timotius.natha@students.utdi.ac.id>
2023-06-02 19:10:38 +0700

    mengganti assets images carousel

--- capstone/lib/main.dart
+++ capstone/lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       getPages: AppPage.list,
-
-      initialRoute: AppRoute.landing_page,
+      initialRoute: AppRoute.cariloker_screen,
       debugShowCheckedModeBanner: false,
       initialBinding: BindingsBuilder(() {
         Get.put<Auth>(Auth());
