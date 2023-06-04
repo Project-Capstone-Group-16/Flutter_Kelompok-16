d64108c18c3b8729c8e985c4672a257e428fe83f
Rainmakers21 <timotius.natha@students.utdi.ac.id>
2023-06-04 20:58:54 +0700

    adding Dashboard Page and fixing Explore Page

--- capstone/lib/view/categoryPage/category_screen.dart
+++ capstone/lib/view/categoryPage/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:capstone/view/cariLoker/cariloker.dart';
 import 'package:flutter/material.dart';
 
 import 'package:capstone/components/color_path.dart';
@@ -12,13 +13,11 @@ class CategoryBarangScreen extends StatefulWidget {
 }
 
 class _CategoryBarangScreenState extends State<CategoryBarangScreen> {
-
-
-  bool isContainer1Active=false;
-  bool isContainer2Active=false;
-  bool isContainer3Active=false;
-  bool isContainer4Active=false;
-  String selectedImage='';
+  bool isContainer1Active = false;
+  bool isContainer2Active = false;
+  bool isContainer3Active = false;
+  bool isContainer4Active = false;
+  String selectedImage = '';
   String? dropdownValue;
 
   @override
@@ -42,55 +41,46 @@ class _CategoryBarangScreenState extends State<CategoryBarangScreen> {
                     Navigator.pop(context);
                   },
                 ),
+              ),
+              const SizedBox(width: 60),
+              const Text(
+                'Kategori Barang',
+                style: TextStyle(
+                  fontSize: 23,
+                  color: ColorPath.textcolor1,
+                  fontWeight: FontWeight.w600,
                 ),
-
-                const SizedBox(width: 60),
-
-                const Text(
-                  'Kategori Barang',
-                  style: TextStyle(
-                    fontSize: 23,
-                    color: ColorPath.textcolor1,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )
-              ],
-            ),
-
-            const SizedBox(height: 30),
-
-            Container(
-              padding: const EdgeInsets.only(top: 45),
+              )
+            ],
+          ),
+          const SizedBox(height: 30),
+          Container(
+            padding: const EdgeInsets.only(top: 45),
+            alignment: Alignment.center,
+            child: Stack(
               alignment: Alignment.center,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    width: 350,
-                    height: 740,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 0.2
-                      ),
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      ),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0, 3),  
-                          blurRadius: 2,        
-                          spreadRadius: 2,      
-                        ),
-                      ],
+              children: [
+                Container(
+                  width: 350,
+                  height: 740,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black, width: 0.2),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
                     ),
-                    
-
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 3),
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
                   child: Column(
                     children: [
                       const SizedBox(height: 20),
@@ -233,56 +223,53 @@ class _CategoryBarangScreenState extends State<CategoryBarangScreen> {
                               bottomRight: Radius.circular(20),
                             ),
                             image: selectedImage.isNotEmpty
-                            ?DecorationImage(image: AssetImage(selectedImage),
-                            fit: BoxFit.cover
-                            ):const DecorationImage(image: AssetImage('assets/images/emptycategoryscreen.png'),
-                            fit: BoxFit.contain
-                            )
+                                ? DecorationImage(
+                                    image: AssetImage(selectedImage),
+                                    fit: BoxFit.cover)
+                                : const DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/emptycategoryscreen.png'),
+                                    fit: BoxFit.contain)),
+                      ),
+                      const SizedBox(height: 20),
+                      Align(
+                        alignment: const AlignmentDirectional(0.05, -0.82),
+                        child: Container(
+                          width: 280,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey, width: 1),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: DropdownButtonFormField<String>(
+                            value: dropdownValue,
+                            items: <String>[
+                              'Kecil',
+                              'Sedang',
+                              'Besar',
+                            ].map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue;
+                              });
+                            },
+                            decoration: const InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 10),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              hintText: 'Jenis Loker',
                             ),
                           ),
-
-                          const SizedBox(height: 20),
-
-                          Align(
-                          alignment: const AlignmentDirectional(0.05, -0.82),
-                          child: Container(
-                            width: 280,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(color: Colors.grey, width: 1),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: DropdownButtonFormField<String>(
-                              value: dropdownValue,
-                              items: <String>[
-                                'Kecil',
-                                'Sedang',
-                                'Besar',
-                              ].map((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  dropdownValue = newValue;
-                                });
-                              },
-                    decoration: const InputDecoration(
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: 'Jenis Loker',
-                    ),
-                  ),
-                ),
-              ),
-
-                          const SizedBox(height: 70),
-
+                        ),
+                      ),
+                      const SizedBox(height: 70),
                       AllButton(
                         onTap: () {
                           Navigator.of(context)
@@ -296,7 +283,6 @@ class _CategoryBarangScreenState extends State<CategoryBarangScreen> {
                       ),
                     ],
                   ),
-                  
                 )
               ],
             ),
